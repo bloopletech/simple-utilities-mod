77383c5c6d741b7330c529f8f878a841106ddbde
Brenton "B-Train" Fletcher <i@bloople.net>
2020-10-14 00:43:44 +1100

    Lighten lightGray colour as existing colour is too dark

--- src/main/java/net/johnvictorfs/simple_utilities/helpers/Colors.java
+++ src/main/java/net/johnvictorfs/simple_utilities/helpers/Colors.java
@@ -1,7 +1,7 @@
 package net.johnvictorfs.simple_utilities.helpers;
 
 public class Colors {
-    public static final int lightGray = Integer.parseInt("9e9e9e", 16);
+    public static final int lightGray = Integer.parseInt("bebebe", 16);
     public static final int lightRed = Integer.parseInt("db4f4f", 16);
     public static final int lightYellow = Integer.parseInt("e0ed53", 16);
     public static final int lightOrange = Integer.parseInt("edb753", 16);
